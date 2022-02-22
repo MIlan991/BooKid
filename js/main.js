861b0b280f6359ef820c50607739e5e43feb3d41
Milan Cica <milan.cica991@gmail.com>
2022-02-22 17:07:41 +0400

    update

--- js/main.js
+++ js/main.js
@@ -40,22 +40,75 @@ $(function() {
 });
 
 //BURGER MENU
-let navBtn = document.getElementById('tgl-btn');
-let closeBtn = document.getElementById('close-tgl')
+const navBtn = document.getElementById('tgl-btn');
+const checkBox = document.getElementById("burger3");
+const navBar = document.querySelector('#myNav');
+const closeNav = document.querySelectorAll('.close-nav');
 
-let tglDiv = document.querySelector('.toggleNav')
-
-let closeNav = document.querySelectorAll('.close-nav');
+// TOGGLEE NAV 
 for (i = 0; i < closeNav.length; i++) {
   closeNav[i].addEventListener('click', () => {
-    tglDiv.style.display = "none";
+    navBar.style.display = "none";
+    checkBox.checked = false;
   });
 }
 
-navBtn.addEventListener('click', () => {
-  tglDiv.style.display = 'block';
-});
+// OPEN AND CLOSE NAVBAR
+function openCloseNav() {
+  if (checkBox.checked == true){
+    navBar.style.display = "block";
+  } else {
+    navBar.style.display = "none";
+  }
+}
 
-closeBtn.addEventListener('click', () => {
-  tglDiv.style.display = 'none';
-});
+//FORMS CHECK
+document.getElementById("registration_form").onsubmit = function () {
+  const nameForm = document.forms["registration_form"]["name"].value;
+  const emailForm = document.forms["registration_form"]["email"].value;
+  const titleForm = document.forms["registration_form"]["telephone"].value;
+  const messageForm = document.forms["registration_form"]["comment"].value;
+  const submitMessage = document.getElementById('button_error');
+
+  let messageError = false;
+  let submit = true;
+
+  if (nameForm == null || nameForm == "") {
+      nameError = "The field is required.";
+      document.getElementById("name_error").innerHTML = nameError;
+      submitMessage.innerHTML = 'One or more fields have an error. Please check and try again.';
+      submit = false;
+  }
+
+  if (emailForm == null || emailForm == "") {
+      emailError = "The field is required.";
+      document.getElementById("email_error").innerHTML = emailError;
+      submitMessage.innerHTML = 'One or more fields have an error. Please check and try again.';
+      submit = false;
+  }
+
+  if (titleForm == null || titleForm == "") {
+      titleError = "The field is required.";
+      document.getElementById("telephone_error").innerHTML = titleError;
+      submitMessage.innerHTML = 'One or more fields have an error. Please check and try again.';
+      submit = false;
+  }
+
+  if (messageForm == null || messageForm == "") {
+      messageError = "The field is required.";
+      document.getElementById("comment_error").innerHTML = messageError;
+      submitMessage.innerHTML = 'One or more fields have an error. Please check and try again.';
+      submit = false;
+  }
+  
+  return submit;
+}
+
+function removeWarning() {
+  document.getElementById(this.id + "_error").innerHTML = "";
+}
+
+document.getElementById("name").onkeyup = removeWarning;
+document.getElementById("email").onkeyup = removeWarning;
+document.getElementById("telephone").onkeyup = removeWarning;
+document.getElementById("comment").onkeyup = removeWarning;
